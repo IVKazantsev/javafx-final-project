172d362a67cd4d825df8b703357d12be543b74d6
Ilya Kazancev <ilyakazancev24112003@gmail.com>
2023-11-28 14:29:29 +0200

    refactor

--- src/Main.java
+++ src/Main.java
@@ -27,19 +27,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException { // Stage - пользовательский интерфейс
 
-        Group root = new Group(); // Корневой узел
-        // Картинка заголовка
+/////////////// Заголовок ///////////////
         Image icon = new Image("file:img/icon.png", 50, 50, true, true);
         ImageView iconView = new ImageView(icon);
 
-        // Заголовок
         Text title = new Text("Todoist");
 
         FlowPane titleContainer = new FlowPane(iconView, title);
-
-        root.getChildren().add(titleContainer);
-
-        // Список дел
+/////////////// Список дел ///////////////
         Group todos = new Group();
         Todo[] todosArray = getTodos();
 
@@ -54,23 +49,14 @@ public class Main extends Application {
             todos.getChildren().add(todo);
             todo.setLayoutY(todos.getLayoutY() + i * 20);
         }
-        root.getChildren().add(todos);
+/////////////// Форма добавления дела ///////////////
 
-        // Ввод нового дела
         TextField input = new TextField("What to do?");
 
-        // Кнопка добавления
         Button saveButton = new Button("Save");
 
-//        int todosCount = getTodos().length; // Подсчет количества тудушек для корректного отображения отступов
-
         FlowPane newTodoForm = new FlowPane(input, saveButton);
-
-        System.out.println(getTodos().length);
-
-        root.getChildren().add(newTodoForm);
-
-        // Ввод нового дела
+/////////////// Копирайт и навигация ///////////////
         Label copyright = new Label("© 2023 Todoist Kazancev Korol");
         // Переход по вкладкам
         Button navButton1 = new Button("-1 day");
@@ -83,24 +69,24 @@ public class Main extends Application {
                 navButton2,
                 navButton3,
                 navButton4);
-
-        titleContainer.setLayoutX(80);    // установка положения надписи по оси X
-        titleContainer.setLayoutY(80);    // установка положения надписи по оси Y
+/////////////// Корневой узел ///////////////
+        Group root = new Group(titleContainer,
+                todos,
+                newTodoForm,
+                footer);
+/////////////// Настройка отступов ///////////////
+        root.setLayoutX(80);
+        titleContainer.setLayoutY(80);
         titleContainer.setHgap(5);
 
-        todos.setLayoutX(80);   // установка положения надписи по оси X
-        todos.setLayoutY(titleContainer.getLayoutY() + titleContainer.getHeight() + 80);    // установка положения надписи по оси Y
+        todos.setLayoutY(titleContainer.getLayoutY() + 80);
 
-        newTodoForm.setLayoutX(80);   // установка положения надписи по оси X
-        newTodoForm.setLayoutY(todos.getLayoutY() + getTodos().length * 20 + 20);    // установка положения надписи по оси Y
+        newTodoForm.setLayoutY(todos.getLayoutY() + getTodos().length * 20 + 20);
         newTodoForm.setHgap(5);
 
-        footer.setLayoutX(80);
         footer.setLayoutY(newTodoForm.getLayoutY() + 30);
         footer.setHgap(5);
-
-        root.getChildren().add(footer);
-
+/////////////// Нажатие на кнопку Save ///////////////
         saveButton.setOnAction(actionEvent -> {
             Todo todo = new Todo(input.getText());
             CheckBox todoCkeckBox = new CheckBox(todo.getTitle());
@@ -140,7 +126,7 @@ public class Main extends Application {
         // Все визуальные элементы помещаем в контейнер, который хранится в сцене
         Scene scene = new Scene(root, Color.rgb(220, 220, 220));
 
-        // Настройка окна
+/////////////// Настройка окна ///////////////
         stage.setScene(scene);
         stage.setTitle("Todoist");
         stage.setWidth(700);
@@ -154,9 +140,7 @@ public class Main extends Application {
 
         try {
             if (todos.createNewFile()) {
-                System.out.println("File created");
-            } else {
-                System.out.println("File already exists");
+                System.out.println("Created new file with todos");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
